7e826a4ce3acc6c6c7584ffb12f46af6bdfde9f2
Самудинова <53327079+azica@users.noreply.github.com>
2024-04-22 11:43:18 +0600

    stopped exchange refetch

--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -1,23 +1,39 @@
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom'; // Assuming you are using React Router
 import { useQuery } from 'react-query';
 import axios from 'axios';
-
-import useTickets from 'shared/hooks/useTickets';
-import { exchangeApiUrl } from 'shared/constants';
 import useConverterPrice from 'shared/hooks/useConverter';
-
 import { TicketCard } from 'components/TicketCard';
 import Skeleton from 'components/Skeleton/Skeleton';
-
 import { Container } from './style';
+import useTickets from 'shared/hooks/useTickets';
+import { exchangeApiUrl } from 'shared/constants';
 
 const Tickets = () => {
     const [searchParams] = useSearchParams();
     const defaultCurrencyCode = 'RUB';
     const [currencyCode, setCurrencyCode] = useState<string>(searchParams.get('currency') || defaultCurrencyCode);
     const [queryString, setQueryString] = useState<string>(searchParams.get('stops') || '');
+
     const { tickets, loading, error } = useTickets(queryString);
+
+    const { data: fetchedRates, refetch: refetchRates } = useQuery<Rates>({
+        queryKey: 'rates',
+        queryFn: async () => {
+            const response = await axios.get<{ conversion_rates: Rates }>(exchangeApiUrl);
+            return response.data.conversion_rates;
+        },
+        enabled: false,
+    });
+
+    useEffect(() => {
+        const fetchRates = async () => {
+            if (currencyCode && !fetchedRates) {
+                await refetchRates();
+            }
+        };
+        fetchRates();
+    }, [currencyCode, fetchedRates, refetchRates]);
 
     useEffect(() => {
         const stopsParam = searchParams.get('stops') || '';
@@ -28,16 +44,7 @@ const Tickets = () => {
         setCurrencyCode(searchParams.get('currency') || defaultCurrencyCode);
     }, [searchParams, defaultCurrencyCode]);
 
-
-    const { data: fetchedRates, isSuccess } = useQuery<Rates>({
-        queryKey: 'rates',
-        queryFn: async () => {
-            const response = await axios.get<{ conversion_rates: Rates }>(exchangeApiUrl);
-            return response.data.conversion_rates;
-        }
-    })
-
-    const convertedPrice: Ticket[] = useConverterPrice(tickets, fetchedRates, "RUB", currencyCode);
+    const convertedPrice: Ticket[] = useConverterPrice(tickets, fetchedRates, 'RUB', currencyCode);
 
     return (
         <Container>
